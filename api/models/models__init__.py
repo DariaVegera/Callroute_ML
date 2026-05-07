a9596d882b56118599bc245de7742abe29c1e5a7
dasvean <122227724+DariaVegera@users.noreply.github.com>
2026-05-08 00:27:31 +0300

    Add files via upload

---
+++ api/models/models__init__.py
@@ -0,0 +1,12 @@
+# Импортируем все модели чтобы Alembic/SQLAlchemy видел metadata
+from api.models.user import User, UserBalance
+from api.models.billing import Transaction, LoyaltyLevel, ReferralCode, ReferralEvent, WeeklyCashbackLog
+from api.models.prediction import PredictionTier, PredictionTask, MLModel
+from api.models.hitl import HITLTask, HITLCompletion
+
+__all__ = [
+    "User", "UserBalance",
+    "Transaction", "LoyaltyLevel", "ReferralCode", "ReferralEvent", "WeeklyCashbackLog",
+    "PredictionTier", "PredictionTask", "MLModel",
+    "HITLTask", "HITLCompletion",
+]
